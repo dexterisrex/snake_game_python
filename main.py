625bebedbdcde1786dfc0175a61609117e2047bf
@dexterisrex <rahulsharma.sharma931@gmail.com>
2021-09-14 17:25:45 +0530

    Moving block with timer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#Converting program into OOPS ie. Classes and Objects
+#Moving block with timer
 import pygame
 from pygame.locals import *
+import time
 
 class Snake:
     def __init__(self,parent_screen):
@@ -8,6 +9,7 @@ class Snake:
         self.block = pygame.image.load("resources/block.jpg").convert()
         self.x = 100
         self.y = 100
+        self.direction = 'down'
 
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
@@ -15,21 +17,27 @@ class Snake:
         pygame.display.flip()    #Updating your screen after any changes
 
     def move_up(self):
-        self.y -= 10
-        self.draw()
+        self.direction = 'up'
     
     def move_down(self):
-        self.y += 10
-        self.draw()
+        self.direction = 'down'
     
     def move_left(self):
-        self.x -= 10
-        self.draw()
+        self.direction = 'left'
 
     def move_right(self):
-        self.x += 10
-        self.draw()
+        self.direction = 'right'
 
+    def walk(self):
+        if self.direction == 'left':
+            self.x -= 10
+        if self.direction == 'right':
+            self.x += 10
+        if self.direction == 'up':
+            self.y -= 10
+        if self.direction == 'down':
+            self.y += 10
+        self.draw()
 
 class Game:
     def __init__(self):
@@ -63,6 +71,9 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
+
+            self.snake.walk()
+            time.sleep(0.2)
 
 if __name__ == '__main__':
     game = Game()
