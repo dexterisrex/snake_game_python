8b1b826ba845d971a118318ca4dd6f5b0fbd5a30
@dexterisrex <rahulsharma.sharma931@gmail.com>
2021-09-14 18:11:36 +0530

    Snake and Apple

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-#Moving block with timer
+#Draw Snake and Apple
 import pygame
 from pygame.locals import *
 import time
 
-class Snake:
+SIZE = 40
+
+class Apple:
     def __init__(self,parent_screen):
+        self.image = pygame.image.load("resources/apple.jpg").convert()
+        self.parent_screen = parent_screen
+        self.x = SIZE*3
+        self.y = SIZE*3
+
+    def draw(self):
+        self.parent_screen.blit(self.image,(self.x, self.y))
+        pygame.display.flip()
+
+class Snake:
+    def __init__(self,parent_screen,length):
+        self.length = length
         self.parent_screen = parent_screen
         self.block = pygame.image.load("resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE]*length
+        self.y = [SIZE]*length
         self.direction = 'down'
 
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
-        self.parent_screen.blit(self.block,(self.x, self.y))   #To draw a block img in surface window 
+        for i in range(self.length):
+            self.parent_screen.blit(self.block,(self.x[i], self.y[i]))   #To draw a block img in surface window 
         pygame.display.flip()    #Updating your screen after any changes
 
     def move_up(self):
@@ -29,14 +44,19 @@ class Snake:
         self.direction = 'right'
 
     def walk(self):
+
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
+
         if self.direction == 'left':
-            self.x -= 10
+            self.x[0] -= SIZE
         if self.direction == 'right':
-            self.x += 10
+            self.x[0] += SIZE
         if self.direction == 'up':
-            self.y -= 10
+            self.y[0] -= SIZE
         if self.direction == 'down':
-            self.y += 10
+            self.y[0] += SIZE
         self.draw()
 
 class Game:
@@ -44,8 +64,14 @@ class Game:
         pygame.init()
         self.surface = pygame.display.set_mode((1000,500))   # Creating a window
         self.surface.fill((110, 110, 5))     #Filling the surface window by using rgb
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 6)
         self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
 
     def run(self):
         running = True
@@ -72,8 +98,9 @@ class Game:
                     running = False
 
 
-            self.snake.walk()
-            time.sleep(0.2)
+            self.play()
+
+            time.sleep(0.3)
 
 if __name__ == '__main__':
     game = Game()
