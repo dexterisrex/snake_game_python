080f769faf8b214d488267ab79fae6839132700a
@dexterisrex <rahulsharma.sharma931@gmail.com>
2021-09-14 16:25:16 +0530

    OOPS concept is used

--- main.py
+++ main.py
@@ -1,48 +1,72 @@
-#Build a screen and block which can move around
+#Converting program into OOPS ie. Classes and Objects
 import pygame
 from pygame.locals import *
 
-def draw_block():
-    surface.fill((255,255,255))
-    surface.blit(block,(block_x, block_y))
-    pygame.display.flip()
+class Snake:
+    def __init__(self,parent_screen):
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("resources/block.jpg").convert()
+        self.x = 100
+        self.y = 100
+
+    def draw(self):
+        self.parent_screen.fill((110, 110, 5))
+        self.parent_screen.blit(self.block,(self.x, self.y))   #To draw a block img in surface window 
+        pygame.display.flip()    #Updating your screen after any changes
+
+    def move_up(self):
+        self.y -= 10
+        self.draw()
+    
+    def move_down(self):
+        self.y += 10
+        self.draw()
+    
+    def move_left(self):
+        self.x -= 10
+        self.draw()
+
+    def move_right(self):
+        self.x += 10
+        self.draw()
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000,500))   # Creating a window
+        self.surface.fill((110, 110, 5))     #Filling the surface window by using rgb
+        self.snake = Snake(self.surface)
+        self.snake.draw()
+
+    def run(self):
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+                        
+                    if event.key == K_UP:
+                        self.snake.move_up()
+                    
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+
+                elif event.type == QUIT:
+                    running = False
+
 
 if __name__ == '__main__':
-    pygame.init()
+    game = Game()
+    game.run()
 
-    surface = pygame.display.set_mode((1000,500))   # Creating a window
-    surface.fill((255,255,255))     #Filling the surface window by using rgb
-
-    block = pygame.image.load("resources/block.jpg").convert()
-    block_x = 100
-    block_y = 100
-    surface.blit(block,(block_x, block_y))   #To draw a block img in surface window 
-
-    pygame.display.flip()   #Updating your screen after any changes
  
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-                    
-                if event.key == K_UP:
-                    block_y -= 10
-                    draw_block()
-                
-                if event.key == K_DOWN:
-                    block_y += 10
-                    draw_block()
-
-                if event.key == K_LEFT:
-                    block_x -= 10
-                    draw_block()
-
-                if event.key == K_RIGHT:
-                    block_x += 10
-                    draw_block()
-
-            elif event.type == QUIT:
-                running = False
+    
